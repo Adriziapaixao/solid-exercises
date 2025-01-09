322b7564beff594072b10e4e30026cadd0888800
adrizia.paixao <adrizia.paixao@zup.com.br>
2025-01-09 17:42:04 -0300

    Adiciona classes e interface implementando o Princípio da Inversão de Dependência (DIP) - Criada a interface DispositivoEntrada com o método usar(), representando dispositivos genéricos de entrada. - Adicionada a classe Teclado implementando DispositivoEntrada com o comportamento "Digitando com o teclado". - Adicionada a classe Mouse implementando DispositivoEntrada com o comportamento "Clicando com o mouse". - Refatorando a classe Computador para depender da abstração (DispositivoEntrada), utilizando injeção de dependência via construtor. - Organizada a classe Main para demonstrar a funcionalidade do sistema com diferentes dispositivos de entrada.

---
+++ src/exercicio5/Mouse.java
@@ -0,0 +1,8 @@
+package src.exercicio5;
+
+public class Mouse implements DispositivoEntrada{
+    @Override
+    public void usar() {
+        System.out.println("Clicando com o mouse...");
+    }
+}
---
+++ src/exercicio5/Teclado.java
@@ -0,0 +1,9 @@
+package src.exercicio5;
+
+public class Teclado implements DispositivoEntrada{
+    @Override
+    public void usar() {
+        System.out.println("Digintando com o teclado...");
+
+    }
+}
